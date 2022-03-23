490d809e2062b9e86307f1876d7c8e3a30af6aaf
sandhikagalih <sandhikagalih@gmail.com>
2022-03-23 16:11:46 +0700

    portfolio tailwind css

---
+++ dist/js/script.js
@@ -0,0 +1,20 @@
+// Navbar Fixed
+window.onscroll = function () {
+  const header = document.querySelector('header');
+  const fixedNav = header.offsetTop;
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add('navbar-fixed');
+  } else {
+    header.classList.remove('navbar-fixed');
+  }
+};
+
+// Hamburger
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function () {
+  hamburger.classList.toggle('hamburger-active');
+  navMenu.classList.toggle('hidden');
+});
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#14b8a6',
+        secondary: '#64748b',
+        dark: '#0f172a',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
